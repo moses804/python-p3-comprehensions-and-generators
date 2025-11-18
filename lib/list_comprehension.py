e5619d39bdfa1af0ab43d54c5f558fa67c7a55c1
Moses Maina <mainacalton57@gmail.com>
2025-11-18 13:18:50 +0300

    lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
-#!/usr/bin/env python3
+# lib/list_comprehension.py
 
-def return_evens(num_list):
-    pass
+def return_evens(seq):
+    """
+    Returns a list of even numbers from the input sequence using a list comprehension.
+    """
+    return [num for num in seq if num % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+
+def make_exclamation(sentences):
+    """
+    Returns a list of sentences with an exclamation mark added at the end of each sentence using a list comprehension.
+    """
+    return [sentence + "!" for sentence in sentences]
